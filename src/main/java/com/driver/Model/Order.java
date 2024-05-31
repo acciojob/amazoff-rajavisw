c4bb2e54264aa9a4040dfbd0e243641a254d0dd8
rajavisw <rajaviswanadhapalli12@gmail.com>
2024-05-31 09:45:30 +0530

    third commit

--- src/main/java/com/driver/Model/Order.java
+++ src/main/java/com/driver/Model/Order.java
@@ -9,7 +9,7 @@ public class Order {
     public Order(String id, String deliveryTime) {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
-        //deliveryTime  = HH*60 + MM
+        //deliveryTime  = HH*60 + MM  ok
         this.id=id;
         //int dTime=Integer.parseInt(deliveryTime.substring(0,2))*60+Integer.parseInt(deliveryTime.substring(3,5));
         this.deliveryTime=deliveryTime;
