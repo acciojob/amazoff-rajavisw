6ac260a2bef836ffae95f4eea859e9cba84b6436
rajavisw <rajaviswanadhapalli12@gmail.com>
2024-05-31 17:26:02 +0530

    sixth commit

--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderService {
 
-    @Autowired
+
     OrderRepository orderRepository = new OrderRepository();
 
     public void addOrder(Order order){
